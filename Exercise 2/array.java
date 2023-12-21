a2a63cdc1c8b1613b0f75062c5865ad7586f7e2a
unknown <kavinduemalka1@gmail.com>
2023-12-21 15:39:46 +0530

    add new Exercise

---
+++ Exercise 2/array.java
@@ -0,0 +1,11 @@
+class array{
+    public static void main(String[] args) {
+        int [] br = new int [10];
+        for (int i = 0 ; i < 10; i++){
+            int sn = i*i;
+            br [i]= sn ;
+        }
+        //System.out.println();
+
+    }
+}
