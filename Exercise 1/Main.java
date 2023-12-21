ae7aac063b9a323b4ced969cecd99a22f4170f04
unknown <kavinduemalka1@gmail.com>
2023-12-21 15:31:37 +0530

    add change2

--- Exercise 1/Main.java
+++ Exercise 1/Main.java
@@ -1,7 +1,7 @@
 public class Main {
  public static void main(String[] args) {
     Car c1 = new Car("Toyota222", "Corolla");
-    Car c2 = new Car("Toyota", "Camry");
+    Car c2 = new Car("Toyota333", "Camry");
 c1.DisplayInfo();
 c2.DisplayInfo(); 
 }    
