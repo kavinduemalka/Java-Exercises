869d9eb6540338d6f1613aaa92a82836734e0c31
unknown <kavinduemalka1@gmail.com>
2023-12-27 22:06:07 +0530

    change 2

--- project1/Student.java
+++ project1/Student.java
@@ -3,15 +3,15 @@ public class Student{
     static Scanner sc = new Scanner(System.in);
 
 public static void main(String[] args) {
-        String[] regno = new String[500];
-        String[] batch = new String[500];
-        String[] name1 = new String[500];
-        String[] name2 = new String[500];
-        Double[] gpa = new Double[500];
+        String[] Regno = new String[500];
+        String[] Batch = new String[500];
+        String[] Name1 = new String[500];
+        String[] Name2 = new String[500];
+        Double[] GPA = new Double[500];
 
         int count = 0; 
 
-          
+        while (true) {
                 System.out.println("-------------------------------------------");
                 System.out.println("   A VOLATILE STUDENT MAINTENANCE SYSTEM   "); 
                 System.out.println("-------------------------------------------"); 
@@ -23,11 +23,57 @@ public static void main(String[] args) {
                 System.out.println("4. Delete a Student Record");
                 System.out.println();
 
-                System.out.print("ENTER OPTION [0-4]");
+                System.out.println("ENTER OPTION [0-4]");
                 int option = sc.nextInt();
                 sc.nextLine();
-}
-}
+
+                if (option == 0) {
+                        System.out.println("Exiting the program.");
+                        System.exit(0);
+                        break;
+                }
+                
+                else if (option == 1) {
+                System.out.print("Enter the batch (14/15/16/17) : ");
+                String batch = sc.nextLine(); 
+                System.out.print("Enter the registration number : "); 
+                String regno = sc.nextLine(); 
+                System.out.print("Enter the first name          : ");
+                String name1 = sc.nextLine(); 
+                System.out.print("Enter the last name           : ");
+                String name2 = sc.nextLine(); 
+                System.out.print("Enter the cumulative GPA      : ");
+                Double gpa = sc.nextDouble(); 
+                sc.nextLine();
+                
+                Regno[count] = regno;
+                    Batch[count] = batch;
+                    Name1[count] = name1;
+                    Name2[count] = name2;
+                    GPA[count] = gpa;
+                    count++;
+                    break;
+                }
+                
+                
+                else if (option == 2) {
+                    System.out.print("Enter the batch (14/15/16/17) : ");
+                    String batch = sc.nextLine(); 
+                    System.out.print("Enter the registration number : "); 
+                    String regno = sc.nextLine();               
+                        for (int i = 0; i < count; i++) {
+                            if (Regno[i].equals(regno) & Batch[i].equals(batch)){
+                                String n1 = Name1[i];
+                                String n2 = Name2[i];
+                                Double G = GPA[i];
+                                    System.out.println("The student "+"n1 "+"n2 "+"E/"+"batch"+"/"+"regno"+"/"+" has a cumulative GPA of "+G);
+                                    break;
+                                }
+                        }
+                    }
+                }
+            }
+        }
+
                 
     
-        
